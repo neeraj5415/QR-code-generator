73bd27ca5af96322d63b59405c279bfe680e5617
Neeraj Kumar <neeraj010701@gmail.com>
2025-08-10 14:24:14 +0530

    Updated my SQL config for deploying site

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+$servername = "sql111.infinityfree.com"; // Your MySQL Host
+$username = "if0_39673432";      // Your InfinityFree DB username
+$password = "Neeraj5415";       // Your InfinityFree DB password
+$dbname = "if0_39673432"; // Your DB name
+
 $conn = new mysqli("localhost", "root", "Neer@j5415", "qr_system");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
