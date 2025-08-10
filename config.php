9d51adf74d2aaaef277833da0e7309d9da3349ec
Neeraj Kumar <neeraj010701@gmail.com>
2025-08-10 14:26:42 +0530

    Updated wrong config data

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $username = "if0_39673432";      // Your InfinityFree DB username
 $password = "Neeraj5415";       // Your InfinityFree DB password
 $dbname = "if0_39673432"; // Your DB name
 
-$conn = new mysqli("localhost", "root", "Neer@j5415", "qr_system");
+$conn = new mysqli("$servername", "$username", "$password", "$dbname");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
